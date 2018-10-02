5f1b5542a228c37a1eb435834396596355220f4e
Otávio Vera Cruz <otavioveracruz@gmail.com>
2018-10-02 01:12:37 -0300

    Add cadastro artista e contratante

--- showUp/app/cadastros/CadastroArtistas.java
+++ showUp/app/cadastros/CadastroArtistas.java
@@ -2,4 +2,20 @@ package cadastros;
 
 public class CadastroArtistas{
 
+	private IRepositorioArtista repositorioArtista;
+
+	public CadastroArtistas(){
+		this.repositorioArtista = new RepositorioArtistaDB();
+	}
+
+	public Artista cadastrar(Artista artista){
+		Artista aux=this.repositorioArtista.cadastrar(artista);
+		return aux;
+	}
+
+	public boolean existeConta(String cpf){
+		boolean aux = this.repositorioArtista.existeConta(cpf);	
+
+	}
+
 }
--- showUp/app/cadastros/CadastroContratantes.java
+++ showUp/app/cadastros/CadastroContratantes.java
@@ -2,4 +2,18 @@ package cadastros;
 
 public class CadastroContratantes{
 
+	private IReposirioContratante repositorioContrante;
+
+	public CadastroContratantes(){
+		this.repositorioContrante = new RepositorioContratanteDB();
+	}
+
+	public Contratante cadastrar(Contratante contratante){
+		Contratante aux=this.repositorioContrante.cadastrar(contratante);
+	}
+
+	public boolean existeConta(String cpf){
+		boolean aux = this.repositorioContrante.existeConta(cpf);
+		return aux;
+	}
 }
